8b2af22970983d74b38417310535bf6775da20e7
tgdh91 <37333340+tgdh91@users.noreply.github.com>
2019-06-03 18:01:02 +0800

    Revert "临时"
    This reverts commit 67051e98005620dcd8364c3e28c685c205deb491.

--- Customer/views.py
+++ Customer/views.py
@@ -1,25 +1,3 @@
 from django.shortcuts import render
 
-from UserDefine.Controller import Controller, controller
-from 温控系统 import models
-from UserDefine.ConfigReader import config_info, DefaultConfig
-
-# -*- coding: UTF-8 -*-
-
-Default = DefaultConfig()
-
-controller = Controller()
-
-
-class Front:
-    def login(request, self)
-
-
-    def getAccount(request, self)
-
-    def logout(request, self)
-
-
-
-
 # Create your views here.
