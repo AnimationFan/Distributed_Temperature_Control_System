1a17805e5af05c3ce6b21b78ccb429466cf72f15
CatalpaW <649618487@qq.com>
2019-06-04 16:35:02 +0800

    修改顾客主页面可以显示目标温度、目标风速、当前温度三项

--- Customer/views.py
+++ Customer/views.py
@@ -16,11 +16,12 @@ Default=DefaultConfig()
 class Customer:
     id=''
     room=''
-    targettem=Default.DefaultTemp
+    targettemp=Default.DefaultTemp
+    targetwind=1
     user=id
 
     def __init__(self):
-        self.targettem=Default.DefaultTemp
+        self.targettemp=Default.DefaultTemp
 
 precus=Customer()
 
@@ -30,8 +31,12 @@ def welcome(request):
     pre = models.UserRoom.object.get(User_name=precus.id)
     preroom=pre.room.room_num
     precus.room=preroom
-    temp=precus.targettem
-    #return render_to_response('welcome.html',{'room':preroom,'temp':temp})
+    last = controller.getStates();
+    for var in last:
+        if var["RoomNum"]==precus.room:
+              lastone=var
+    currenttemp=lastone['Temp']
+    return render_to_response('welcome.html',{'room':preroom,'targettemp':precus.targettemp,'currenttemp':currenttemp,'targetwind':precus.targetwind})
 
     #开启空调
 @login_required
@@ -43,13 +48,10 @@ def TurnOn(request):
     #设置温度
 def setTemp(request):
     global controller
-    last = controller.getStates();
-    for var in last:
-        if var["RoomNum"]==precus.room:
-              lastone=var
     t = request.GET['temp']
     t = int(t)
-    w=lastone['Temp']
+    precus.targettemp = t
+    w=precus.targetwind
     controller.setAirCState(precus.room,t,w,precus.id)
     HttpResponseRedirect("/Customer/")
 
@@ -57,13 +59,10 @@ def setTemp(request):
 @login_required
 def setWind(request):
     global controller
-    last = controller.getStates();
-    for var in last:
-      if var["RoomNum"] == precus.room:
-         lastone = var
     w = request.GET['wind']
     w = int(w)
-    t=lastone["Temp"]
+    precus.targetwind = w
+    t=precus.targettemp
     controller.setAirCState(precus.room,t,w,precus.id)
     HttpResponseRedirect("/Customer/")
 
